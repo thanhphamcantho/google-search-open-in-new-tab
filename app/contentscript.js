0f7fd00165036e8dc45e93aef50b0d8004ae4020
Vinoth Kumar S <vinoth@contus.in>
2019-09-02 09:39:04 +0530

    Version 2.1.0 - Stack Overflow added

--- app/contentscript.js
+++ app/contentscript.js
@@ -1,34 +1,55 @@
+'use strict';
 
+const url = new URL(window.location.href);
+const domain = url.hostname;
 
-var url = new URL(window.location.href)
-var domain = url.hostname
-var q = url.searchParams.get("q");
-var googleDomain = domain.includes("google");
+const path = '/search';
+const queryParams = url.searchParams.get('q');
 
-if (googleDomain && q && document.querySelectorAll('a > h3')) {
+const googleDomain = domain.includes('google');
+const stackOverflowDomain = domain.includes('stackoverflow');
 
-    for (i = 0; i < document.querySelectorAll('a > h3').length; i++) {
-        document.querySelectorAll('a > h3')[i].parentElement.setAttribute('target', '_blank');
-        document.querySelectorAll('a > h3')[i].innerHTML += '&nbsp;<svg style="position:relative;top:2px;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M19 19H5V5h7V3H3v18h18v-9h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/></svg>';
-    }
+if (googleDomain && queryParams && url.pathname === path) {
+  const openNewTabIcon =
+    '<svg style="position:relative; top:5px;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M19 19H5V5h7V3H3v18h18v-9h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/></svg>';
 
+  const openNewTabIcon0px =
+    '<svg style="position:relative; top:0px;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M19 19H5V5h7V3H3v18h18v-9h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/></svg>';
 
-    for (i = 0; i < document.querySelectorAll('h3 > a').length; i++) {
-        document.querySelectorAll('h3 > a')[i].setAttribute('target', '_blank');
-        
-        document.querySelectorAll('h3 > a')[i].innerHTML += '&nbsp;<svg style="position:relative;top:2px;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M19 19H5V5h7V3H3v18h18v-9h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/></svg>';
-    }
+  const svgWithSpace = `&nbsp; ${openNewTabIcon}`;
+  const h3TitleTags = document.querySelectorAll('a > h3');
+  const titleAnchorTags = document.querySelectorAll('h3 > a');
+  const anchorTags = document.querySelectorAll('a');
 
-    for (i = 0; i < document.querySelectorAll('g-link > a').length; i++) {
-        document.querySelectorAll('g-link > a')[i].setAttribute('target', '_blank');
-    }
+  for (const h3TitleTag of h3TitleTags) {
+    h3TitleTag.parentElement.setAttribute('target', '_blank');
+    h3TitleTag.innerHTML += svgWithSpace;
+  }
 
-    for (i = 0; i < document.querySelectorAll('g-inner-card > a').length; i++) {
-        document.querySelectorAll('g-inner-card > a')[i].setAttribute('target', '_blank');
-    }
+  for (const titleAnchorTag of titleAnchorTags) {
+    titleAnchorTag.setAttribute('target', '_blank');
+    titleAnchorTag.innerHTML += `&nbsp; ${openNewTabIcon0px}`;
+  }
 
-    for (i = 0; i < document.querySelectorAll('g-inner-card > div > a').length; i++) {
-        document.querySelectorAll('g-inner-card > div > a')[i].setAttribute('target', '_blank');
-    }
+  for (const anchorTag of anchorTags) {
+    anchorTag.setAttribute('target', '_blank');
+  }
+}
 
+if (stackOverflowDomain) {
+  const openNewTabIcon =
+    '<svg style="position:relative; top:2px;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M19 19H5V5h7V3H3v18h18v-9h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/></svg>';
+
+  const svgWithSpace = `&nbsp; ${openNewTabIcon}`;
+  const titleAnchorTags = document.querySelectorAll('h3 > a');
+  const anchorTags = document.querySelectorAll('a');
+
+  for (const titleAnchorTag of titleAnchorTags) {
+    titleAnchorTag.setAttribute('target', '_blank');
+    titleAnchorTag.innerHTML += svgWithSpace;
+  }
+
+  for (const anchorTag of anchorTags) {
+    anchorTag.setAttribute('target', '_blank');
+  }
 }
